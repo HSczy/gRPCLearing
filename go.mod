f030c3b510a89e1224c35873e76b44c410bd93ae
HSczy <hsczyic@gmail.com>
2021-07-15 23:41:13 +0800

    更新文件

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/HSczy/GRPCLearing
+
+go 1.16
+
+require google.golang.org/grpc v1.39.0 // indirect
--- route-server/server.go
+++ route-server/server.go
@@ -1,1 +1,18 @@
-package route_server
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	pb "github.com/HSczy/gRPCLearning/route"
+)
+
+func main{
+	lis, err := net.Listen("tcp", "localhost:5000")
+	if err != nil {
+		log.Fatalln("cannot create a listener at the address")
+	}
+	grpcServer := grpc.NewServer()
+	pb.
+
+}
